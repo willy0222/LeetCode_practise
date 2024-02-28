f9d1c328d8367c2599b321879110fe058be8004a
willy0222 <137746549+willy0222@users.noreply.github.com>
2024-02-28 11:41:00 +0800

    Create 0182.Duplicate Emails.sql

---
+++ problems/0182.Duplicate Emails.sql
@@ -0,0 +1,25 @@
+Example 1:
+
+Input: 
+Person table:
++----+---------+
+| id | email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+Output: 
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+Explanation: a@b.com is repeated two times.
+
+
+# Write your MySQL query statement below
+select email as Email
+from Person
+group by email
+having count(email) > 1
