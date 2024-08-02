bd191ffc55eec2e6d88be04af67e3d5300b98b7c
willy0222 <137746549+willy0222@users.noreply.github.com>
2024-08-02 09:24:15 +0800

    Create 0584. Find Customer Referee.sql

---
+++ problems/database/0584. Find Customer Referee.sql
@@ -0,0 +1,29 @@
+Example 1:
+
+Input: 
+Customer table:
++----+------+------------+
+| id | name | referee_id |
++----+------+------------+
+| 1  | Will | null       |
+| 2  | Jane | null       |
+| 3  | Alex | 2          |
+| 4  | Bill | null       |
+| 5  | Zack | 1          |
+| 6  | Mark | 2          |
++----+------+------------+
+Output: 
++------+
+| name |
++------+
+| Will |
+| Jane |
+| Bill |
+| Zack |
++------+
+
+
+# Write your MySQL query statement below
+select name
+from Customer
+where referee_id != 2 or referee_id is null
