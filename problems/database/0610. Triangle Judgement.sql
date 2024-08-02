8db088545e1e67542d6c799f418d1abaf1e3792f
willy0222 <137746549+willy0222@users.noreply.github.com>
2024-08-02 10:07:54 +0800

    Update 0610. Triangle Judgement.sql

--- problems/database/0610. Triangle Judgement.sql
+++ problems/database/0610. Triangle Judgement.sql
@@ -1,5 +1,11 @@
-Example 1:
+Report for every three line segments whether they can form a triangle.
 
+Return the result table in any order.
+
+The result format is in the following example.
+
+Example 1:
+```sql
 Input: 
 Triangle table:
 +----+----+----+
@@ -15,8 +21,8 @@ Output:
 | 13 | 15 | 30 | No       |
 | 10 | 20 | 15 | Yes      |
 +----+----+----+----------+
-
-
+```
+```sql
 # Write your MySQL query statement below
 select x, y, z, 
 case 
@@ -24,3 +30,4 @@ case
     else 'No'
 end as triangle
 from Triangle
+```
