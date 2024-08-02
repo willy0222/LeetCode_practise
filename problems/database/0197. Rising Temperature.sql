8197807ba4cc43dd00e6d61f813ec72a516b020a
willy0222 <137746549+willy0222@users.noreply.github.com>
2024-08-02 09:12:53 +0800

    Create 0197. Rising Temperature.sql

---
+++ problems/database/0197. Rising Temperature.sql
@@ -0,0 +1,29 @@
+Example 1:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+
+
+# Write your MySQL query statement below
+SELECT a.id
+FROM Weather a
+JOIN Weather b ON a.recordDate = DATE_ADD(b.recordDate, INTERVAL 1 DAY)
+WHERE a.temperature > b.temperature;
