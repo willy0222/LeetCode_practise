cbcaa3db920993b1a0a9b6b18e92414564407c63
willy0222 <137746549+willy0222@users.noreply.github.com>
2024-08-02 09:53:19 +0800

    Create 1484. Group Sold Products By The Date.sql

---
+++ problems/database/1484. Group Sold Products By The Date.sql
@@ -0,0 +1,35 @@
+Example 1:
+
+Input: 
+Activities table:
++------------+------------+
+| sell_date  | product     |
++------------+------------+
+| 2020-05-30 | Headphone  |
+| 2020-06-01 | Pencil     |
+| 2020-06-02 | Mask       |
+| 2020-05-30 | Basketball |
+| 2020-06-01 | Bible      |
+| 2020-06-02 | Mask       |
+| 2020-05-30 | T-Shirt    |
++------------+------------+
+Output: 
++------------+----------+------------------------------+
+| sell_date  | num_sold | products                     |
++------------+----------+------------------------------+
+| 2020-05-30 | 3        | Basketball,Headphone,T-shirt |
+| 2020-06-01 | 2        | Bible,Pencil                 |
+| 2020-06-02 | 1        | Mask                         |
++------------+----------+------------------------------+
+Explanation: 
+For 2020-05-30, Sold items were (Headphone, Basketball, T-shirt), we sort them lexicographically and separate them by a comma.
+For 2020-06-01, Sold items were (Pencil, Bible), we sort them lexicographically and separate them by a comma.
+For 2020-06-02, the Sold item is (Mask), we just return it.
+
+
+# Write your MySQL query statement below
+select sell_date, 
+count(distinct product) as num_sold, 
+group_concat(distinct product order by product ASC separator ',') as products
+from Activities
+group by sell_date
