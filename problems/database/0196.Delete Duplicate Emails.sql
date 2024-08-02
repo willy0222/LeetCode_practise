73afdf3892e9c06ab7ee27970d5e83aa9e3fec15
willy0222 <137746549+willy0222@users.noreply.github.com>
2024-08-02 09:06:04 +0800

    Create 0196.Delete Duplicate Emails.sql

---
+++ problems/database/0196.Delete Duplicate Emails.sql
@@ -0,0 +1,23 @@
+Example 1:
+
+Input: 
+Person table:
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+Output: 
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+Explanation: john@example.com is repeated two times. We keep the row with the smallest Id = 1.
+
+# Write your MySQL query statement below
+delete p2 from Person p1, Person p2
+where p1.Email = p2.Email and p2.Id > p1.Id;
