3d7a15aaba04f4c2b3c8b3f35ac7146d5d7736b6
willy0222 <137746549+willy0222@users.noreply.github.com>
2024-08-02 09:13:59 +0800

    Create 0577. Employee Bonus.sql

---
+++ problems/database/0577. Employee Bonus.sql
@@ -0,0 +1,34 @@
+Example 1:
+
+Input: 
+Employee table:
++-------+--------+------------+--------+
+| empId | name   | supervisor | salary |
++-------+--------+------------+--------+
+| 3     | Brad   | null       | 4000   |
+| 1     | John   | 3          | 1000   |
+| 2     | Dan    | 3          | 2000   |
+| 4     | Thomas | 3          | 4000   |
++-------+--------+------------+--------+
+Bonus table:
++-------+-------+
+| empId | bonus |
++-------+-------+
+| 2     | 500   |
+| 4     | 2000  |
++-------+-------+
+Output: 
++------+-------+
+| name | bonus |
++------+-------+
+| Brad | null  |
+| John | null  |
+| Dan  | 500   |
++------+-------+
+
+
+# Write your MySQL query statement below
+select name, bonus
+from Employee as e left join Bonus as b
+on e.empId = b.empId
+where bonus < 1000 or bonus is null
