52fc38d801a714f7bc192ce74ba11b878519b68e
willy0222 <137746549+willy0222@users.noreply.github.com>
2024-08-02 09:25:42 +0800

    Create 0610. Triangle Judgement.sql

---
+++ problems/database/0610. Triangle Judgement.sql
@@ -0,0 +1,26 @@
+Example 1:
+
+Input: 
+Triangle table:
++----+----+----+
+| x  | y  | z  |
++----+----+----+
+| 13 | 15 | 30 |
+| 10 | 20 | 15 |
++----+----+----+
+Output: 
++----+----+----+----------+
+| x  | y  | z  | triangle |
++----+----+----+----------+
+| 13 | 15 | 30 | No       |
+| 10 | 20 | 15 | Yes      |
++----+----+----+----------+
+
+
+# Write your MySQL query statement below
+select x, y, z, 
+case 
+    when x + y > z and x + z > y and y + z > x then 'Yes'
+    else 'No'
+end as triangle
+from Triangle
