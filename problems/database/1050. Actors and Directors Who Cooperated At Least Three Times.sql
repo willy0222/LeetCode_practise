d333960c756d9268ce054982ed4199434620ff15
willy0222 <137746549+willy0222@users.noreply.github.com>
2024-08-02 09:41:20 +0800

    Create 1050. Actors and Directors Who Cooperated At Least Three Times.sql

---
+++ problems/database/1050. Actors and Directors Who Cooperated At Least Three Times.sql
@@ -0,0 +1,29 @@
+Example 1:
+
+Input: 
+ActorDirector table:
++-------------+-------------+-------------+
+| actor_id    | director_id | timestamp   |
++-------------+-------------+-------------+
+| 1           | 1           | 0           |
+| 1           | 1           | 1           |
+| 1           | 1           | 2           |
+| 1           | 2           | 3           |
+| 1           | 2           | 4           |
+| 2           | 1           | 5           |
+| 2           | 1           | 6           |
++-------------+-------------+-------------+
+Output: 
++-------------+-------------+
+| actor_id    | director_id |
++-------------+-------------+
+| 1           | 1           |
++-------------+-------------+
+Explanation: The only pair is (1, 1) where they cooperated exactly 3 times.
+
+
+# Write your MySQL query statement below
+select actor_id, director_id
+from ActorDirector
+group by actor_id, director_id
+having count(*) >= 3
